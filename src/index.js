9580ab8b88433ddf448f10216a7aff8cf507fa17
Omar Gutierrez Condori <omargc.mus@gmail.com>
2019-10-16 00:38:40 -0400

    scss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './assets/scss/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
